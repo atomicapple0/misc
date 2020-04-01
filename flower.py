7816ee6855a03999114aabaced74accbf12e5d45
atomicapple <brianzhangaa@gmail.com>
2020-04-01 13:47:10 -0400

    initial commit

---
+++ flower.py
@@ -0,0 +1,56 @@
+import pprint
+
+
+master = [[1,1,1,0,0,1,1],[1,1,1,0,1,0,0,1],[1,1,0,0,0,0],[1,0,1,1,1,1,0],[1,0,1,0,1,1,1],[1,0,1,1,0,1,0,1,0],[1,0]]
+l = ['HE','DE','SU','LA','F','SH','K','A','GRA','BO','S','AQ','S','SE','C','S','E','TA','X','L','V','D','UR','E','E','AMI','C','Y','OB','UIT','U','C','L','H','P','R','L','DV','I','E','E','R','L','K','CR','L','A','E','A','A','E','A','R','S','A','I','ESM','LIS','S','AT','N','LE','AZ','U','IN','M','D','M','B','CT','I','IG','TZ','EN','E','H','IN','E','S','Y','E','E','E','A','UP','AN','S','ED','N','S']
+n = 90
+
+pluckornot = master[6]
+plucked = [0]*n
+print(plucked)
+
+def allplucked(plucked):
+  for x in plucked:
+    if(x==0):
+      return False
+  return True
+
+inc = True
+i = 0
+j = -1
+k = -1
+m = -1
+o = 0
+while(not allplucked(plucked)):
+  if(plucked[i] == 0 and i != m):
+    k = (k+1) % len(pluckornot)
+    m = i
+    o = 0
+    if(pluckornot[k] == 1):
+      print("plucked:" + str(i))
+      j = i
+      plucked[i] = 1
+
+  if(i == n-1):
+    inc = False
+  if(i == 0):
+    inc = True
+
+  if(inc):
+    i += 1
+  else:
+    i -= 1
+
+
+  o += 1
+  if(o>3*n):
+    break
+
+pprint.pprint(plucked)
+for i in range(len(plucked)):
+  if(plucked[i] == 0):
+    print("the answer is " + str(i))
+    print(l[i])
+print("lol then answer is " + str(j))
+print(l[j])
+
---
+++ pokecord.py
@@ -0,0 +1,37 @@
+import time, sys, re
+from pyautogui import press, typewrite, hotkey
+
+#p!market search --name wingull --order price a
+#$p add 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+
+"""
+Example input arguement
+PokécordBOTToday at 10:55 PM
+Pokécord Market:
+Level 16 Wingull | ID: 362585589 | Price: 1 Credits
+Level 16 Wingull | ID: 367224111 | Price: 1 Credits
+Level 40 Wingull | ID: 367833153 | Price: 1 Credits
+Level 9 Wingull | ID: 353484162 | Price: 3 Credits
+Level 22 Wingull | ID: 193645436 | Price: 4 Credits
+Level 4 Wingull | ID: 206872445 | Price: 5 Credits
+Level 23 Wingull | ID: 208117809 | Price: 5 Credits
+...
+"""
+
+arg = sys.argv[1]
+
+m = re.findall('ID: (\d{5,10})', arg)
+
+
+time.sleep(3)
+
+for i in m:
+    typewrite('p!market buy ' + i)
+    press('enter')
+    time.sleep(.7)
+    typewrite('p!confirmbuy')
+    press('enter')
+    time.sleep(.7)
+
+
+
