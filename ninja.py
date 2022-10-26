998ccb846759ef736a13c3513dc62e28e4049d43
atomicapple <brianzhangaa@gmail.com>
2022-10-25 22:31:38 -0400

    nice

--- ninja.py
+++ ninja.py
@@ -4,12 +4,14 @@ sentence = "ni *jaasdasd ninjaasdas **nja n** j*"
 kk = len(word)
 best = 0
 for i in range(len(sentence)):
+    print("start: ", end="")
     j = i
     k = 0
     n_stars = 0
     n_spaces = 0
     while k < kk and i < len(sentence):
-        if sentence[i] == " ":
+        print(sentence[i], end = "")
+        if k > 0 and sentence[i] == " ":
             n_spaces += 1
             i += 1
         elif sentence[i] == "*":
@@ -21,7 +23,10 @@ for i in range(len(sentence)):
             i += 1
         else:
             break
+    score = 0
     if k == kk and n_spaces > 0:
-        best = 1 + n_stars
+        score = 1 + n_stars
+        best = max(best, score)
+    print(f" (score: {score})")
 
 print(best)
